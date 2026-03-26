61cd58f477eb4c7e8924e3a6c054eb3602bb94a5
PulkitAg13 <pulkit.ag13@gmail.com>
2026-03-26 15:45:50 +0530

    Update llm.py

--- Backend/ai/llm.py
+++ Backend/ai/llm.py
@@ -1,15 +1,29 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-OLLAMA_URL = "http://localhost:11434/api/generate"
+# Load env variables
+load_dotenv()
+
+API_KEY = os.getenv("GROQ_API_KEY")
 
 def ask_llama(prompt):
-    response = requests.post(
-        OLLAMA_URL,
-        json={
-            "model": "llama3",
-            "prompt": prompt,
-            "stream": False
-        }
-    )
+    try:
+        response = requests.post(
+            "https://api.groq.com/openai/v1/chat/completions",
+            headers={
+                "Authorization": f"Bearer {API_KEY}",
+                "Content-Type": "application/json"
+            },
+            json={
+                "model": "llama3-8b-8192",
+                "messages": [
+                    {"role": "user", "content": prompt}
+                ]
+            }
+        )
 
-    return response.json()["response"]
+        return response.json()["choices"][0]["message"]["content"]
+
+    except Exception as e:
+        return f"Error: {str(e)}"
