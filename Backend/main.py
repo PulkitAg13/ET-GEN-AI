37d60251e1e607fa70c064c6b4511701aeb0e4b4
PulkitAg13 <pulkit.ag13@gmail.com>
2026-03-26 11:29:16 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from db.database import engine, Base
 
 from routes import user, finance, advisor, tax
@@ -7,6 +8,13 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(user.router, prefix="/user")
 app.include_router(finance.router, prefix="/finance")
 app.include_router(advisor.router, prefix="/advisor")
